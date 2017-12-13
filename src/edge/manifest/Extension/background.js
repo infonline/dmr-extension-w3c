eb4a999f354b8ebf2eb757b24b8aca69d125a868
Jens Oswald <joswald@infonline.de>
2017-12-13 14:17:10 +0100

    fix(edge): load iam.js in edge via INFOnline sourcer abd use callback instead of promise

--- src/edge/manifest/Extension/background.js
+++ src/edge/manifest/Extension/background.js
@@ -15,15 +15,11 @@ try {
   browser.runtime.onMessage.addListener(function (response, sender) {
     console.log(response, sender);
     var tabId = sender.tab.id;
-    // Because of errors in the remote iam script we have to load the
-    //
-    browser.tabs.executeScript(tabId, {file: 'iam.js'})
-      .then(function () {
-        return browser.tabs.executeScript(tabId, {file: 'count.js'})
-      })
-      .catch(function (err) {
-        console.error(err);
+    loadScript("https://script.ioam.de/iam.js", function (code) {
+      browser.tabs.executeScript(tabId, {code: code}, function() {
+        browser.tabs.executeScript(tabId, {file: 'count.js'});
       });
+    });
   });
 } catch (err) {
   console.error(err);
