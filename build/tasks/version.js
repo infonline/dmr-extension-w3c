1bce44a5b3d9471979a02b4b9a5a65a3ab805637
Jens Oswald <joswald@infonline.de>
2017-12-16 00:47:26 +0100

    chore(build): remove git commit from version task

--- build/tasks/version.js
+++ build/tasks/version.js
@@ -12,14 +12,11 @@
  * introduced a feature or made a backwards-incompatible release.
  */
 import gulp from 'gulp';
-import git from 'gulp-git';
 import bump from 'gulp-bump';
 import filter from 'gulp-filter';
-import semver from 'semver';
 import conventionalChangelog from 'gulp-conventional-changelog';
 import pump from 'pump';
 import tagVersion from 'gulp-tag-version';
-import packageDetails from '../../package.json';
 
 /**
  * Increments the version of the whole repository by incrementing the versions in
@@ -28,29 +25,25 @@ import packageDetails from '../../package.json';
  * @param {String} importance - Patch, minor, or major
  * @param {Function} next - Callback
  */
-const increment = (importance, next) => {
-  const newVersion = semver.inc(packageDetails.version, importance);
-  return pump([
-    // Get all the files to bump version in
-    gulp.src(['package.json', 'src/shared/manifest.json'], { base: './' }),
-    // Bump the version number in those files
-    bump({ type: importance }),
-    // Save it back to filesystem
-    gulp.dest('./'),
-    // Get changelog for generating the change log
-    gulp.src('CHANGELOG.md', { base: './' }),
-    // Write conventional change log
-    conventionalChangelog({ preset: 'angular' }),
-    // Save it back to filesystem
-    gulp.dest('./'),
-    // Commit the changed version number
-    git.commit(`chore(release): v${newVersion}`),
-    // Read only one file to get the version number
-    filter('package.json'),
-    // Tag it in the repository
-    tagVersion(),
-  ], next);
-};
+const increment = (importance, next) => pump([
+  // Get all the files to bump version in
+  gulp.src(['package.json', 'src/shared/manifest.json'], { base: './' }),
+  // Bump the version number in those files
+  bump({ type: importance }),
+  // Save it back to filesystem
+  gulp.dest('./'),
+  // Get changelog for generating the change log
+  gulp.src('CHANGELOG.md', { base: './' }),
+  // Write conventional change log
+  conventionalChangelog({ preset: 'angular' }),
+  // Save it back to filesystem
+  gulp.dest('./'),
+  // Read only one file to get the version number
+  filter('package.json'),
+  // Tag it in the repository
+  tagVersion(),
+], next);
+
 gulp.task('bump:patch', next => increment('patch', next));
 gulp.task('bump:minor', next => increment('minor', next));
 gulp.task('bump:major', next => increment('major', next));
