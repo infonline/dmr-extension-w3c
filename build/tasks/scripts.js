d4033f7b836b77cbaa335ff6df6ba398b5c39736
Jens Oswald <joswald@infonline.de>
2017-12-15 22:11:41 +0100

    chore(build): integrate webpack bundler in script task
    With this change it's possible to import 3rd party libraries to every javascript source.
    We need this to stay DRY and promisify the chrome webextension api.

--- build/tasks/scripts.js
+++ build/tasks/scripts.js
@@ -1,17 +1,56 @@
 import gulp from 'gulp';
-import gulpif from 'gulp-if';
-import babel from 'gulp-babel';
-import sourcemaps from 'gulp-sourcemaps';
-import uglify from 'gulp-uglify';
+import { log, colors } from 'gulp-util';
+import named from 'vinyl-named';
+import webpack from 'webpack';
+import gulpWebpack from 'webpack-stream';
 import pump from 'pump';
+import template from 'gulp-template';
 import args from '../lib/args';
 import { multiVendorPath } from '../lib/vendors';
 
+const ENV = args.production ? 'production' : 'development';
+/**
+ * Webpack configuration
+ * @type {Object}
+ */
+const config = {
+  devtool: args.sourcemaps ? 'inline-source-map' : false,
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(ENV),
+      'process.env.VENDOR': JSON.stringify(args.vendor),
+    }),
+  ],
+  module: {
+    rules: [{
+      parser: {
+        amd: false,
+      },
+    }, {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      options: {
+        presets: args.production ? ['minify'] : [],
+      },
+    }],
+  },
+  watch: args.watch,
+};
+
 gulp.task('scripts', next => pump([
   gulp.src(multiVendorPath(args.vendor, 'scripts/**/*.js')),
-  gulpif(args.sourcemaps, sourcemaps.init()),
-  babel(),
-  gulpif(args.production, uglify()),
-  gulpif(args.sourcemaps, sourcemaps.write()),
+  named(),
+  gulpWebpack(config, webpack, (err, stats) => {
+    if (err) {
+      return;
+    }
+    log(`Finished '${colors.cyan('scripts')}'`, stats.toString({
+      chunks: false,
+      colors: true,
+      cached: false,
+      children: false,
+    }));
+  }),
+  template({ vendor: args.vendor }),
   gulp.dest(`dist/${args.vendor}/scripts`),
 ], next));
