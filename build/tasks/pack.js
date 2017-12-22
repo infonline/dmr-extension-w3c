709c7f9cd57bbae6e08f128be0a1827216498140
Jens Oswald <joswald@infonline.de>
2017-12-22 16:00:23 +0100

    chore(build): make a condition for edge browser in pack tasks
    With this change an edge-based extension will be packed according to the microsoft schema. See https://docs.microsoft.com/en-us/microsoft-edge/extensions/guides/packaging/creating-and-testing-extension-packages for details.

--- build/tasks/pack.js
+++ build/tasks/pack.js
@@ -8,23 +8,34 @@
  * gulp pack    # Will pack the build vendor specific web extension into a package file
  */
 import gulp from 'gulp';
-import { colors, log } from 'gulp-util';
 import zip from 'gulp-zip';
 import pump from 'pump';
 import packageDetails from '../../package.json';
 import args from '../lib/args';
 import { getPackageFileType } from '../lib/vendors';
+import edgeManifestReplace from '../lib/edge';
 
 gulp.task('pack', ['build'], (next) => {
   const { name, version } = packageDetails;
   const fileType = getPackageFileType(args.vendor);
   const filename = `${name}-${version}-${args.vendor}${fileType}`;
-  const distStyled = colors.green(`dist/${args.vendor}`);
-  const filenameStyled = colors.gree(`./packages/${filename}`);
+  if (args.vendor === 'edge') {
+    return pump([
+      gulp.src('build/assets/edge/**/*'),
+      edgeManifestReplace('dist/edge/manifest.json'),
+      gulp.dest('.temp'),
+    ], pump([
+      gulp.src('dist/edge/**/*'),
+      gulp.dest('.temp/Extension'),
+    ]), pump([
+      gulp.src('.temp/**/*'),
+      zip(filename),
+      gulp.dest('./packages'),
+    ], next));
+  }
   return pump([
     gulp.src(`dist/${args.vendor}/**/*`),
     zip(filename),
     gulp.dest('./packages'),
-    log(`Created web extension package ${distStyled} to ${filenameStyled}`),
   ], next);
 });
