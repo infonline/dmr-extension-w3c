c227e31ec38e9ec71c9e4a8ab14d0000b85fd957
Jens Oswald <joswald@infonline.de>
2017-12-22 15:54:14 +0100

    chore(build): add babel polyfill to allow async/await syntax in build tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,6 @@
+// Import babel polyfill to allow async/await in code
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'babel-polyfill';
 import requireDir from 'require-dir';
 
 // Load all build tasks
