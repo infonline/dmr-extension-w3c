1d07044ed582e8eab5e3bc0ad9015faaefa05f3e
Jens Oswald <joswald@infonline.de>
2018-01-15 09:36:20 +0100

    chore(build): remove gulp from build pipeline

--- gulpfile.babel.js
+++
@@ -1,7 +0,0 @@
-// Import babel polyfill to allow async/await in code
-// eslint-disable-next-line import/no-extraneous-dependencies
-import 'babel-polyfill';
-import requireDir from 'require-dir';
-
-// Load all build tasks
-requireDir('./build/tasks');
