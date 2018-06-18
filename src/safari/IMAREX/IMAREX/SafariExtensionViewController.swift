acc23f156f7726ea40cfc21db59cedf6b399438d
Jens Oswald <joswald@infonline.de>
2018-06-18 11:35:25 +0200

    feat(src): scaffold safari extension

---
+++ src/safari/IMAREX/IMAREX/SafariExtensionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SafariExtensionViewController.swift
+//  IMAREX
+//
+//  Created by Oswald, Jens on 18.06.18.
+//
+
+import SafariServices
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
+    
+    static let shared = SafariExtensionViewController()
+
+}
