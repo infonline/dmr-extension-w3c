deeca4fb0567084190e7cb401ec949bd0b8db4d1
Jens Oswald <oswald@infonline.de>
2019-05-22 20:35:23 +0200

    feat(store): refactor statistic module
    - save any request (count and event) to the local storage and remove
      them when they are 1 month old
    - aggregate browsing events
    - modify mutation to meet new requirements

--- src/w3c/store/modules/statistic.js
+++ src/w3c/store/modules/statistic.js
@@ -6,12 +6,24 @@ const SAVE = 'SAVE';
 
 const defaultState = () => ({
   statistic: {
+    requests: [],
     sites: {},
-    types: {},
+    events: {},
     createdAt: new Date().toJSON(),
     updatedAt: undefined,
   },
 });
+/**
+ * Finds the last index in the request collection which date is older than one month from now
+ *
+ * @param {Array} requests - Collection of requests
+ * @return {*|number|never} The index of the first element which meets the condition
+ */
+const findLastRequestIndex = (requests) => {
+  const now = new Date();
+  const last = now.setMonth(now.getMonth() - 1);
+  return requests.findIndex(request => Date.parse(request.date) < last);
+};
 
 const actions = {
   /**
@@ -43,42 +55,57 @@ const actions = {
    * Updates the statistics dictionary
    *
    * @param {Function} commit  - Commits mutations to the state triggered by the action
-   * @param state - Current module state
-   * @param {String} [type = 'type' | type = 'site'] - The dictionary type
-   * @param {String} key - The key of the dictionary entry
+   * @param {Object} state - Current module state
+   * @param {String} type = 'type' | type = 'site' - The dictionary type
+   * @param {String} event - Name of the event
+   * @param {String} site - The key of the dictionary entry
    * @returns {Promise<void>} Void
    */
-  async update({ commit, state }, { type, key }) {
+  async update({ commit, state }, { type, event, site }) {
     const localState = await driver.storage.local.get();
     if (localState.statistic && Object.keys(localState.statistic).includes('createdAt')) {
       state.statistic = localState.statistic;
     }
     const { statistic } = state;
-    if (type === 'site') {
+    // Create aggregated data
+    if (type === 'count') {
       const { sites } = statistic;
-      if (!sites[key]) {
-        sites[key] = 0;
+      if (!sites[site]) {
+        sites[site] = 0;
       }
-      sites[key] += 1;
+      sites[site] += 1;
       statistic.updatedAt = new Date().toJSON();
-      await driver.storage.local.set({
-        ...state,
-        statistic,
-      });
-      commit(SAVE, statistic);
-    } else if (type === 'type') {
-      const { types } = statistic;
-      if (!types[key]) {
-        types[key] = 0;
+    } else if (type === 'navigation') {
+      const { events } = statistic;
+      if (!events[event]) {
+        events[event] = 0;
       }
-      types[key] += 1;
-      statistic.updatedAt = new Date().toJSON();
-      await driver.storage.local.set({
-        ...state,
-        statistic,
-      });
-      commit(SAVE, statistic);
+      events[event] += 1;
     }
+    // Save the request
+    let { requests } = statistic;
+    if (!requests) {
+      // eslint-disable-next-line no-param-reassign
+      requests = [];
+    }
+    const lastAllowedRequestIndex = findLastRequestIndex(requests);
+    if (lastAllowedRequestIndex > -1) {
+      requests = requests.slice(lastAllowedRequestIndex, requests);
+    }
+    const request = {
+      event,
+      site,
+      type,
+      date: new Date().toJSON(),
+    };
+    requests.push(request);
+    statistic.requests = requests;
+    statistic.updatedAt = new Date().toJSON();
+    await driver.storage.local.set({
+      ...state,
+      statistic,
+    });
+    commit(SAVE, statistic);
   },
 };
 
