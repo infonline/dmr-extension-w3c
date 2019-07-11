adcdf405a93d8ac5d257e10912df45c170dab991
Jens Oswald <oswald@infonline.de>
2019-07-11 14:32:52 +0200

    feat(build): add working packaging module to build process
    This will pack the bundled extension sources to crx or zip file. The
    result can be later published to the browser extension store.

--- build/pack.mjs
+++ build/pack.mjs
@@ -1,8 +1,11 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+/* eslint-disable import/no-extraneous-dependencies */
 import archiver from 'archiver';
+import del from 'del';
 import fs, { promises as fsPromises } from 'fs';
 import path from 'path';
-import { version } from '../package.json';
+import pkg from '../package.json';
+
+const { version } = pkg;
 
 const BROWSERS = [
   {
@@ -19,32 +22,72 @@ const BROWSERS = [
     extension: 'crx',
   },
 ];
-// Create a archived package from the bundled extension source for each browser vendor
-BROWSERS.forEach(async (BROWSER) => {
-  // Define input path
-  const inputPath = path.join(import.meta.url, '..', 'dist', 'bundle');
-  // Define a versioned output path
-  const outputPathVersioned = path.join(import.meta.url, '..', 'dist', 'packages', BROWSER.name, `DRM-${version}.${BROWSER.extension}`);
-  // Define a latest output path
-  const outputPathLatest = path.join(import.meta.url, '..', 'dist', 'packages', BROWSER.name, `DRM-latest.${BROWSER.extension}`);
-  // Create output directory
-  await fsPromises.mkdir(path.dirname(outputPathVersioned), { recursive: true });
-  // Create output stream
-  const outputStream = fs.createWriteStream(outputPathVersioned);
-  // Create a listener for the end event of nodejs stream api and copy the result to a latest file
-  outputStream.on('end', async () => {
-    await fsPromises.copyFile(outputPathVersioned, outputPathLatest);
+/**
+ * Packs the extension by provided browser
+ *
+ * @param {Object} browser - The browser which will hold the packaging instructions
+ * @return {Promise<void>} Void
+ */
+const pack = async (browser) => {
+  try {
+    // current directory
+    const dirname = path.dirname(import.meta.url.slice(7, import.meta.url.length));
+    // Define input path
+    const inputPath = path.join(dirname, '..', 'dist', 'bundle');
+    // Define a versioned output path
+    const outputPathVersioned = path.join(dirname, '..', 'dist', 'packages', browser.name, `DMR-${version}.${browser.extension}`);
+    // Create output directory
+    await fsPromises.mkdir(path.dirname(outputPathVersioned), { recursive: true });
+    // Recopy all content before start archiving
+    await del(`${path.dirname(outputPathVersioned)}/*`);
+    // Create output stream
+    const outputStream = fs.createWriteStream(outputPathVersioned);
+    // Create new archive instance
+    const archive = archiver('zip', { zlib: { level: 9 } });
+    // good practice to catch this error explicitly
+    archive.on('error', (err) => {
+      throw err;
+    });
+    // pipe archive data to the file
+    archive.pipe(outputStream);
+    // Add bundle directory to archive
+    archive.directory(inputPath, false);
+    // Finalize archive
+    archive.finalize();
+  } catch (error) {
+    throw error;
+  }
+};
+
+/**
+ * Makes a "latest" copy of the packed extension
+ *
+ * @param {Object} browser - The browser which will hold the packaging instructions
+ * @return {Promise<void>} Void
+ */
+const copy = async (browser) => {
+  try {
+    // current directory
+    const dirname = path.dirname(import.meta.url.slice(7, import.meta.url.length));
+    // Define a versioned output path
+    const outputPathVersioned = path.join(dirname, '..', 'dist', 'packages', browser.name, `DMR-${version}.${browser.extension}`);
+    // Define a latest output path
+    const outputPathLatest = path.join(dirname, '..', 'dist', 'packages', browser.name, `DMR-latest.${browser.extension}`);
+    return fsPromises.copyFile(outputPathVersioned, outputPathLatest);
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Start packaging
+Promise
+  .all(BROWSERS.map(BROWSER => pack(BROWSER)))
+  .then(() => Promise.all(BROWSERS.map(BROWSER => copy(BROWSER))))
+  .then(() => {
+    console.info('\nPackaging was successful...\n');
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
   });
-  // Create new archive instance
-  const archive = archiver('zip', { zlib: { level: 9 } });
-  // good practice to catch this error explicitly
-  archive.on('error', (err) => {
-    throw err;
-  });
-  // pipe archive data to the file
-  archive.pipe(outputStream);
-  // Add bundle directory to archive
-  archive.directory(inputPath, false);
-  // Finalize archive
-  archive.finalize();
-});
