c79330a9c6e2989262f6adf2eb3e8dc5b42e37b2
Jens Oswald <oswald@infonline.de>
2019-07-11 15:15:17 +0200

    fix(build): fix empty packages output in pack build step

--- build/pack.mjs
+++ build/pack.mjs
@@ -22,13 +22,15 @@ const BROWSERS = [
     extension: 'zip',
   },
 ];
+
 /**
+ /**
  * Packs the extension by provided browser
  *
  * @param {Object} browser - The browser which will hold the packaging instructions
- * @return {Promise<void>} Void
+ * @return {Promise<any>} Promise
  */
-const pack = async (browser) => {
+const pack = browser => new Promise((resolve, reject) => {
   try {
     // current directory
     const dirname = path.dirname(import.meta.url.slice(7, import.meta.url.length));
@@ -36,18 +38,33 @@ const pack = async (browser) => {
     const inputPath = path.join(dirname, '..', 'dist', 'bundle');
     // Define a versioned output path
     const outputPathVersioned = path.join(dirname, '..', 'dist', 'packages', browser.name, `DMR-${version}.${browser.extension}`);
+    const outputPathLatest = path.join(dirname, '..', 'dist', 'packages', browser.name, `DMR-latest.${browser.extension}`);
     // Create output directory
-    await fsPromises.mkdir(path.dirname(outputPathVersioned), { recursive: true });
+    fs.mkdirSync(path.dirname(outputPathVersioned), { recursive: true });
     // Recopy all content before start archiving
-    await del(`${path.dirname(outputPathVersioned)}/*`);
+    del.sync(`${path.dirname(outputPathVersioned)}/*`);
     // Create output stream
     const outputStream = fs.createWriteStream(outputPathVersioned);
+    // Resolve promise when output stream is ended
+    outputStream.on('finish', () => {
+      fsPromises
+        .copyFile(outputPathVersioned, outputPathLatest)
+        .then(() => resolve())
+        .catch(error => reject(error));
+    });
     // Create new archive instance
     const archive = archiver('zip', { zlib: { level: 9 } });
-    // good practice to catch this error explicitly
-    archive.on('error', (err) => {
-      throw err;
+    // It's a good practice to catch warning for stat failures and another non blocking errors so we
+    // bind a listener to the warning event emitted from node js stream module
+    archive.on('warning', (err) => {
+      if (err.code === 'ENOENT') {
+        console.warn(err.code, err.message);
+      } else {
+        reject(err);
+      }
     });
+    // good practice to catch this error explicitly
+    archive.on('error', error => reject(error));
     // pipe archive data to the file
     archive.pipe(outputStream);
     // Add bundle directory to archive
@@ -55,34 +72,13 @@ const pack = async (browser) => {
     // Finalize archive
     archive.finalize();
   } catch (error) {
-    throw error;
+    reject(error);
   }
-};
-
-/**
- * Makes a "latest" copy of the packed extension
- *
- * @param {Object} browser - The browser which will hold the packaging instructions
- * @return {Promise<void>} Void
- */
-const copy = async (browser) => {
-  try {
-    // current directory
-    const dirname = path.dirname(import.meta.url.slice(7, import.meta.url.length));
-    // Define a versioned output path
-    const outputPathVersioned = path.join(dirname, '..', 'dist', 'packages', browser.name, `DMR-${version}.${browser.extension}`);
-    // Define a latest output path
-    const outputPathLatest = path.join(dirname, '..', 'dist', 'packages', browser.name, `DMR-latest.${browser.extension}`);
-    return fsPromises.copyFile(outputPathVersioned, outputPathLatest);
-  } catch (error) {
-    throw error;
-  }
-};
+});
 
 // Start packaging
 Promise
   .all(BROWSERS.map(BROWSER => pack(BROWSER)))
-  .then(() => Promise.all(BROWSERS.map(BROWSER => copy(BROWSER))))
   .then(() => {
     console.info('\nPackaging was successful...\n');
     process.exit(0);
