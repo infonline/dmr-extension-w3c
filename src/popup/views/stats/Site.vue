ddb0b5460019bb3ee87484f42abe566541151a5e
Jens Oswald <oswald@infonline.de>
2019-07-15 13:55:10 +0200

    feat(popup): add basic view implementation for the new statistic views
    - add basic site view implementation
    - add basic overall view implementation

---
+++ src/popup/views/stats/Site.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container
+    class="pa-2"
+    fluid
+    grid-list-md
+  >
+    <v-layout
+      fluid
+      row
+      wrap
+    >
+      <v-flex xs12>
+        <v-card
+          class="elevation-2"
+          height="250"
+        >
+          <v-card-title>Site</v-card-title>
+        </v-card>
+      </v-flex>
+      <v-flex xs6>
+        <v-card
+          class="elevation-2"
+          height="230"
+        >
+          <v-card-title>Site</v-card-title>
+        </v-card>
+      </v-flex>
+      <v-flex xs6>
+        <v-card
+          class="elevation-2"
+          height="230"
+        >
+          <v-card-title>Site</v-card-title>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<script type="text/javascript" src="./Site.js"></script>
